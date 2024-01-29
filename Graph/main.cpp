d0c0bd728067f8738e40a18bc9b769f384724512
unknown <arthursbdantas@gmail.com>
2024-01-28 23:04:36 -0300

    implementation of weighted graph

--- Graph/main.cpp
+++ Graph/main.cpp
@@ -8,7 +8,6 @@ using namespace std;
 #define maxY 5.0
 
 
-
 #if defined(_WIN32)
 #define WIN32_LEAN_AND_MEAN
 #define VC_EXTRALEAN
@@ -31,6 +30,52 @@ void get_terminal_size(int& width, int& height) {
 #endif // Windows/Linux
 }
 
+class Point {
+    public:
+        double x, y;
+        Point() {};
+        Point(double x, double y): x(x), y(y) {};
+};
+
+void drawPoint(double x, double y, double val) {
+    int width, height;
+    get_terminal_size(width, height);
+    mvprintw(height*(y - minY)/(maxY - minY),
+                width *(x - minX)/(maxX - minX), "(%.0f)", val);
+}
+
+void drawPoints(Point* p, int length) {
+    int width, height;
+    get_terminal_size(width, height);
+    for (int i = 0; i < length; i++) {
+        mvaddch(height*(p[i].y - minY)/(maxY - minY),
+                width *(p[i].x - minX)/(maxX - minX),i+1+0x30);
+    }
+}
+
+void drawLine(Point p0, Point pf, char c) {
+    int width, height;
+    get_terminal_size(width, height);
+    int x0 = width *(p0.x - minX)/(maxX - minX);
+    int y0 = height*(p0.y - minY)/(maxY - minY);
+    int xf = width *(pf.x - minX)/(maxX - minX);
+    int yf = height*(pf.y - minY)/(maxY - minY);
+    int t = max(abs(yf-y0),abs(xf-x0));
+    for (int i = 0; i<t; i++) {
+        mvaddch(y0+i*(yf-y0)/t,
+                x0+i*(xf-x0)/t,c);
+    }
+}
+
+double sqrt(double x) {
+    double tol = 1e-8*x;
+    double x0 = x;
+    while (x0-x/x0>tol || x0-x/x0<-tol) {
+        x0 -= (x0-x/x0)/2;
+    }
+    return x0;
+}
+
 template <class T>
 class LinkedList {
     public:
@@ -45,7 +90,7 @@ class LinkedList {
             return true;
         }
         ~Node() {
-            if (next) delete next;
+            if (next) {delete next;};
         }
     };
     Node *start;
@@ -78,10 +123,12 @@ class Graph {
     public:
     int length;
     LinkedList<int> *adjacencyList;
+    LinkedList<float> *weightList;
+    bool weighted = false;
     Graph() {};
     Graph(string str) {
-        //1 4 -1 0 2 4 -1 1 3 -1 2 4 5 -1 0 1 3 -1 3 -2
-        //equivale a [[1,4],[0,2,4],[1,3],[2,4,5],[0,1,3],[3]]
+        // 1 4 -1 0 2 4 -1 1 3 -1 2 4 5 -1 0 1 3 -1 3 -2
+        // equivale a [[1,4],[0,2,4],[1,3],[2,4,5],[0,1,3],[3]]
         // adjacencyList = new Node<Node<int>>();
         length = 0;
         for (int i = 0; i < str.length(); i++) {if (str[i]=='-') length++;}
@@ -108,53 +155,114 @@ class Graph {
             else number = number*10 + (sign?-1:1)*(str[i]-0x30);
         }
     }
+    Graph(string str, string weights): Graph(str) {
+        weighted = true;
+        length = 0;
+        for (int i = 0; i < weights.length(); i++) {if (weights[i]=='-') length++;}
+        weightList = new LinkedList<float>[length];
+        LinkedList<float> temp = LinkedList<float>();
+        int number = 0;
+        bool sign = false;
+        int count = 0;
+        for (int i = 0; i < weights.length(); i++) {
+            if (weights[i] == -2) break;
+            else if (weights[i] == ' ') {
+                if (number == -1) {
+                    weightList[count] = temp;
+                    temp = LinkedList<float>();
+                    count++;
+                }
+                else {
+                    cout;
+                    temp.insert(number);
+                }
+                number = 0;
+                sign = false;
+            }
+            else if (weights[i] == '-') sign = true;
+            else number = number*10 + (sign?-1:1)*(weights[i]-0x30);
+        }
+        
+    }
     void printList() {
         for (int i = 0; i < length; i++) {
-            adjacencyList[i].print();
+            // adjacencyList[i].print();
+            // weightList[i].print();
             if (i<length-1) cout << "\n";
         }
     }
+
+    void draw() {
+        Point *points = new Point[length];
+        for (int i = 0; i < length; i++) {
+            points[i].x = (rand()%100)/100.0;
+            points[i].y = (rand()%100)/100.0;
+        }
+
+
+        double k_elas = .02;
+        double k_elet = .1;
+        double k_grav = .01;
+        double comprimento = 1;
+        initscr();
+        for (int it = 0; it < 1000; it++) {
+            for (int i = 0; i < length; i++) {         
+                points[i].x -= k_grav * sqrt(points[i].x * points[i].x + points[i].y * points[i].y) * points[i].x;
+                points[i].y -= k_grav * sqrt(points[i].x * points[i].x + points[i].y * points[i].y) * points[i].y;
+                LinkedList<int>::Node* temp = adjacencyList[i].start;
+                while(temp) {
+                    int j = temp->data;
+                    double d = (points[i].x-points[j].x)*(points[i].x-points[j].x) + (points[i].y-points[j].y)*(points[i].y-points[j].y);
+                    d = sqrt(d) - 1;
+                    points[i].x -= k_elas*(points[i].x-points[j].x)/d;
+                    points[i].y -= k_elas*(points[i].y-points[j].y)/d;
+                    temp = temp->next;
+                }
+                for (int j = 0; j < length; j++) {
+                    if (i == j) continue;
+                    double d = (points[i].x-points[j].x)*(points[i].x-points[j].x) + (points[i].y-points[j].y)*(points[i].y-points[j].y);
+                    points[i].x += k_elet*(points[i].x-points[j].x)/d;
+                    points[i].y += k_elet*(points[i].y-points[j].y)/d;
+                }
+            }
+            {
+                clear();
+                int ligacao = 0;
+                for (int i = 0; i < length; i++) {
+                    LinkedList<int>::Node* temp = adjacencyList[i].start;
+                    LinkedList<float>::Node* temp2 = weightList[i].start;
+                    
+                    while(temp) {
+                        int j = temp->data;
+                        if (i<j) {
+                            drawLine(points[i],points[j], ".,#-_=+*/"[ligacao]);
+                            ligacao+=1;
+                            cout << ligacao;
+                        }
+                        temp = temp->next;
+                        temp2 = temp2->next;
+                    }
+                    temp = adjacencyList[i].start;
+                    temp2 = weightList[i].start;
+                    
+                    while(temp) {
+                        int j = temp->data;
+                        if (weighted) drawPoint((points[i].x+points[j].x)/2, (points[i].y+points[j].y)/2, temp2->data);
+                        temp = temp->next;
+                        temp2 = temp2->next;
+                    }
+                }
+                drawPoints(points, length);
+            }
+            refresh();
+        }
+            getchar();
+        endwin();
+
+ 
+    }
 };
 
-class Point {
-    public:
-        double x, y;
-        Point() {};
-        Point(double x, double y): x(x), y(y) {};
-};
-
-
-void drawPoints(Point* p, int length) {
-    int width, height;
-    get_terminal_size(width, height);
-    for (int i = 0; i < length; i++) {
-        mvaddch(height*(p[i].y - minY)/(maxY - minY),
-                width *(p[i].x - minX)/(maxX - minX),i+1+0x30);
-    }
-}
-
-void drawLine(Point p0, Point pf) {
-    int width, height;
-    get_terminal_size(width, height);
-    int x0 = width *(p0.x - minX)/(maxX - minX);
-    int y0 = height*(p0.y - minY)/(maxY - minY);
-    int xf = width *(pf.x - minX)/(maxX - minX);
-    int yf = height*(pf.y - minY)/(maxY - minY);
-    int t = max(abs(yf-y0),abs(xf-x0));
-    for (int i = 0; i<t; i++) {
-        mvaddch(y0+i*(yf-y0)/t,
-                x0+i*(xf-x0)/t,'.');
-    }
-}
-
-double sqrt(double x) {
-    double tol = 1e-8*x;
-    double x0 = x;
-    while (x0-x/x0>tol || x0-x/x0<-tol) {
-        x0 -= (x0-x/x0)/2;
-    }
-    return x0;
-}
 
 int main() {
     // LinkedList<int> l = LinkedList<int>();
@@ -166,56 +274,18 @@ int main() {
     // LinkedList<int> *l2 = new LinkedList<int>[20];
     // l2[0] = l;
 
-    Graph g = Graph("1 4 -1 0 2 4 -1 1 3 -1 2 4 5 -1 0 1 3 -1 3 -2");
-    // g.printList();
 
-    Point *points = new Point[g.length];
-    for (int i = 0; i < g.length; i++) {
-        points[i].x = (rand()%100)/100.0;
-        points[i].y = (rand()%100)/100.0;
-    }
+    // 1 5
+    // 0 2 6
+    // 1 3
+    // 2 4 6
+    // 3 5 6
+    // 0 4
+    // 1 3 4
+    Graph g = Graph("1 5 -1 0 2 6 -1 1 3 -1 2 4 6 -1 3 5 6 -1 0 4 -1 1 3 4 -2", "28 10 -1 28 16 14 -1 16 12 -1 12 22 18 -1 22 25 24 -1 10 25 -1 14 24 -2");
+    g.printList();
+    g.draw();
+    // g.draw();
 
-
-    double k_elas = .02;
-    double k_elet = .1;
-    double k_grav = .01;
-    double comprimento = 1;
-    initscr();
-    for (int it = 0; it < 2000; it++) {
-        for (int i = 0; i < g.length; i++) {         
-            points[i].x -= k_grav * sqrt(points[i].x * points[i].x + points[i].y * points[i].y) * points[i].x;
-            points[i].y -= k_grav * sqrt(points[i].x * points[i].x + points[i].y * points[i].y) * points[i].y;
-            LinkedList<int>::Node* temp = g.adjacencyList[i].start;
-            while(temp) {
-                int j = temp->data;
-                double d = (points[i].x-points[j].x)*(points[i].x-points[j].x) + (points[i].y-points[j].y)*(points[i].y-points[j].y);
-                d = sqrt(d) - 1;
-                points[i].x -= k_elas*(points[i].x-points[j].x)/d;
-                points[i].y -= k_elas*(points[i].y-points[j].y)/d;
-                temp = temp->next;
-            }
-            for (int j = 0; j < g.length; j++) {
-                if (i == j) continue;
-                double d = (points[i].x-points[j].x)*(points[i].x-points[j].x) + (points[i].y-points[j].y)*(points[i].y-points[j].y);
-                points[i].x += k_elet*(points[i].x-points[j].x)/d;
-                points[i].y += k_elet*(points[i].y-points[j].y)/d;
-            }
-        }
-        {
-            clear();
-            for (int i = 0; i < g.length; i++) {
-                LinkedList<int>::Node* temp = g.adjacencyList[i].start;
-                while(temp) {
-                    int j = temp->data;
-                    if (i<j) drawLine(points[i],points[j]);
-                    temp = temp->next;
-                }
-            }
-            drawPoints(points, g.length);
-        }
-        refresh();
-    }
-    endwin();
-
-    return 0;
+   return 0;
 }
